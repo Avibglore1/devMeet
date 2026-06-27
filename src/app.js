7c25e7c94af8a5a645206d10fecca13d7061907c
Avinash <avinashkumarofficial95@gmail.com>
2026-06-27 18:23:49 +0530

    feat: initialize backend with Express and basic server setup
    - Added package.json with project metadata and dependencies
    - Created src/app.js to set up a simple Express server
    - Configured server to respond with a message on root request

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import connectDB from "./config/db.js";
 
 const app = express();
 
 app.use((req,res)=>{
     res.send("From server to u")
 })
-app.listen(4000, ()=>{
+
+connectDB().then(()=>{
+    console.log("Database connected");
+    app.listen(4000, ()=>{
     console.log(`server is listening at 4000`);
+})
+}).catch(err=>{
+    console.error("Database not connected");
 })
